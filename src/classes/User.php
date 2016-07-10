c1dfa071fae0e68ad0679d8ab9c426c9d62f4165
Mike Oram Front End Developer <mike.oram@mayden.co.uk>
2016-07-10 16:51:38 +0100

    begin user login

---
+++ src/classes/User.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Bence;
+
+class User
+{
+
+    public function login($email, $password) {
+        return false;
+    }
+}
--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,7 @@
 <?php
 // Routes
 
+//     return $response->withStatus(200)->withHeader('Location', '/'); // redirect
 
 $app->get('/', function ($request, $response, $args) {
     return $this->renderer->render($response, 'index.phtml', $args);
@@ -8,6 +9,23 @@ $app->get('/', function ($request, $response, $args) {
 
 $app->get('/login', function ($request, $response, $args) {
     $args['breadcrumbs'] = ['/'=>'Home', '/login'=>'Login'];
-
     return $this->renderer->render($response, 'login.phtml', $args);
+});
+
+
+$app->post('/login', function ($request, $response, $args) {
+    $data = $request->getParsedBody();
+    $email = filter_var($data['email'], FILTER_SANITIZE_STRING);
+    $password = $data['password'];
+    $user = new \Bence\User();
+    $loggedIn = $user->login($email, $password);
+
+    if ($loggedIn) {
+        return $this->renderer->render($response, 'login.phtml', $args);
+    }
+
+    $args['breadcrumbs'] = ['/'=>'Home', '/login'=>'Login'];
+    $args['login'] = false;
+    return $this->renderer->render($response, 'login.phtml', $args);
+
 });
