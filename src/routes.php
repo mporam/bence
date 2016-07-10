7a55a438b7e6476dd00d6f49d0f7675afe01c443
Mike Oram Front End Developer <mike.oram@mayden.co.uk>
2016-07-10 15:58:55 +0100

    remove comments

--- src/routes.php
+++ src/routes.php
@@ -3,11 +3,9 @@
 
 
 $app->get('/', function ($request, $response, $args) {
-    // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
 });
 
 $app->get('/login', function ($request, $response, $args) {
-    // Render index view
     return $this->renderer->render($response, 'login.phtml', $args);
 });
