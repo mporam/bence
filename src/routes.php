16857e57db2049af9bb701b35dc7c4e16eda984d
Mike Oram Front End Developer <mike.oram@mayden.co.uk>
2016-07-10 15:50:29 +0100

    create templates and add login route

--- src/routes.php
+++ src/routes.php
@@ -1,14 +1,13 @@
 <?php
 // Routes
 
-//require __DIR__ . '/../src/classes/dbConnector.php';
 
-$app->get('/[{name}]', function ($request, $response, $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
-
-    $db = $this->db; // for testing
-
+$app->get('/', function ($request, $response, $args) {
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
+});
+
+$app->get('/login', function ($request, $response, $args) {
+    // Render index view
+    return $this->renderer->render($response, 'login.phtml', $args);
 });
